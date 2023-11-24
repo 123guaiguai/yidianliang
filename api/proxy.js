4a02ee0fe5b85ffb030a2bdc5978241b50f59a21
zuoshaodao <1952664675@qq.com>
2023-11-24 23:24:10 +0800

    修改vercel的跨域配置

--- api/proxy.js
+++ api/proxy.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     if (req.url.startsWith('/api')) {
-        target = 'http://8.130.92.216:8081'
+        target = process.env.VUE_APP_BASE_URL
     }
 
     // 创建代理对象并转发请求
